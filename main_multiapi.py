46d15c575ec15dac649198abad2cc5877df9e4d9
Nick Roussis <52631245+neek8044@users.noreply.github.com>
2023-03-18 12:59:09 +0200

    Added some cool stuff
    Updated main.py to the latest api version (v9), and probably some other stuff I don't remember. I also created a multiapi script based on the original that iterates through v6 to v9 APIs in case a code does not work on one API but does on another. This way you do not miss any codes but it is slower and strains your internet.

--- main_multiapi.py
+++ main_multiapi.py
@@ -9,12 +9,14 @@ from dhooks import Webhook
 
 
 ## CONFIGURATION
-API_VER = 9
+API_VERS = [6, 7, 8, 9]
 DISPLAY_PROCESSES = False
 WEBHOOK_NAME = "V4C Scanner"
 STARTED_DESCRIPTION = "Started scanning process."
 FOUND_DESCRIPTION = "Nitro Located!"
 MESSAGE = "Link: "
+loadchar = ">"
+loadspace = "-"
 
 
 colorama.init()
@@ -91,18 +93,19 @@ with open("./GeneratedNitroCodes.txt") as file:
 
 		# Get nitro link
         nitro = line.strip("\n")
-        url = f"https://discordapp.com/api/v{API_VER}/entitelemnts/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
-        r = requests.get(url)
+        for version in range(len(API_VERS)):
+            url = f"https://discordapp.com/api/v{API_VERS[version]}/entitelemnts/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
+            r = requests.get(url)
 
-		# Check if nitro is valid
-        if r.status_code == 200:
-            print(str(i) + GREEN + " Found: " + nitro + RESET) if DISPLAY_PROCESSES else ...
-            with open("./v4c_nitro_codes.txt", "w") as f:
-                f.write(nitro + "\n")
-                f.close()
-            sendWebhook(nitro)
-        else:
-            print(str(i) + YELLOW + " Invalid:" + nitro + RESET) if DISPLAY_PROCESSES else print(f"{i}/{num} [{round(i / num * 100)}%]", end="\r")
-            continue
+		    # Check if nitro is valid
+            if r.status_code == 200:
+                print(str(i) + GREEN + " Found: " + nitro + RESET) if DISPLAY_PROCESSES else ...
+                with open("./v4c_nitro_codes.txt", "w") as f:
+                    f.write(nitro + "\n")
+                    f.close()
+                sendWebhook(nitro)
+            else:
+                print(str(i) + YELLOW + " Invalid: " + nitro + RESET) if DISPLAY_PROCESSES else print(f"{i}/{num} [{round(i / num * 100)}%] {loadchar*(version+1)}{loadspace*(len(API_VERS)-version-1)}", end="\r")
+                continue
 
-print("\nLooped through", num, "codes. If any were found they should be stored on a local file (v4c_nitro_codes.txt)")
+print("\nLooped through", num, "codes. If any were found they should be stored on a local file (v4c_nitro_codes.txt) or sent to your webhook.")
